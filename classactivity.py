77c156fbfd0dc87d96c8e882e3179d3aab1bdbcc
sangaybuddha <02230224.cst@rub.edu.bt>
2024-03-03 23:53:02 -0800

    Adding classactivity.py file

---
+++ classactivity.py
@@ -0,0 +1,11 @@
+Name = input("Enter your name:")
+print("Hello", Name)
+num1 = float(input("Enter a number:"))
+num2 = float(input("Enter a number:"))
+product = num1 * num2
+print("The product is:" ,product)
+sum = num1 + num2 
+print("The sum is:", sum)
+difference = num1 % num2
+print("The differenceis:",difference)
+
