f418b7700ac19a54d128d6a5b048e896a46cebce
sangaybuddha <02230224.cst@rub.edu.bt>
2024-05-16 15:14:44 +0600

    classwork.py

---
+++ classwork.py
@@ -0,0 +1,17 @@
+class Dog:
+    def __init__(self, name, colour, age):
+        self.name = name
+        self.colour = colour
+        self.age = age
+dog1 = Dog("Dawa", "brown", 2)
+dog2 = Dog("Nima", "grey", 4) 
+dog3 = Dog("Blacky", "black", 5)
+print(dog1.name)
+print(dog1.colour)
+print(dog1.age)
+print(dog2.name)
+print(dog2.colour)
+print(dog2.age)
+print(dog3.name)
+print(dog3.colour)
+print(dog3.age)
